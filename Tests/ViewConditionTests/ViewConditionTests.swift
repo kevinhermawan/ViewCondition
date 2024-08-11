b4499072f264a5cd57d3b0e81ec982890bfafbfc
Kevin Hermawan <84965338+kevinhermawan@users.noreply.github.com>
2024-08-12 04:17:02 +0700

    feat: adds `canImport` condition (#1)

--- Tests/ViewConditionTests/ViewConditionTests.swift
+++ Tests/ViewConditionTests/ViewConditionTests.swift
@@ -22,6 +22,34 @@ final class ViewConditionTests: XCTestCase {
         XCTAssertEqual(text, "Original")
     }
     
+    func testIfOSModifier() throws {
+        let view = Text("Original").if(os: .iOS) { text in
+            Text("iOS")
+        }
+        
+#if os(iOS)
+        let text = try view.inspect().text().string()
+        XCTAssertEqual(text, "iOS")
+#else
+        let text = try view.inspect().text().string()
+        XCTAssertEqual(text, "Original")
+#endif
+    }
+    
+    func testIfCanImportModifier() throws {
+        let view = Text("Original").if(canImport: .uiKit) { text in
+            Text("UIKit")
+        }
+        
+#if canImport(UIKit)
+        let text = try view.inspect().text().string()
+        XCTAssertEqual(text, "UIKit")
+#else
+        let text = try view.inspect().text().string()
+        XCTAssertEqual(text, "Original")
+#endif
+    }
+    
     func testVisibleModifierTrue() throws {
         let view = Text("Visible").visible(if: true)
         
@@ -44,6 +72,30 @@ final class ViewConditionTests: XCTestCase {
         }
     }
     
+    func testVisibleOnOS() throws {
+        let view = Text("Visible").visible(on: .iOS)
+        
+#if os(iOS)
+        XCTAssertNoThrow(try view.inspect().text())
+#else
+        XCTAssertThrowsError(try view.inspect().text()) { error in
+            XCTAssertTrue(error is InspectionError)
+        }
+#endif
+    }
+    
+    func testVisibleIfCanImport() throws {
+        let view = Text("Visible").visible(ifCanImport: .uiKit)
+        
+#if canImport(UIKit)
+        XCTAssertNoThrow(try view.inspect().text())
+#else
+        XCTAssertThrowsError(try view.inspect().text()) { error in
+            XCTAssertTrue(error is InspectionError)
+        }
+#endif
+    }
+    
     func testHideModifierTrue() throws {
         let view = Text("Hidden").hide(if: true)
         
@@ -64,5 +116,29 @@ final class ViewConditionTests: XCTestCase {
         XCTAssertThrowsError(try view.inspect().text()) { error in
             XCTAssertTrue(error is InspectionError)
         }
+    }
+    
+    func testHideOnOS() throws {
+        let view = Text("Hidden").hide(on: .iOS)
+        
+#if os(iOS)
+        XCTAssertThrowsError(try view.inspect().text()) { error in
+            XCTAssertTrue(error is InspectionError)
+        }
+#else
+        XCTAssertNoThrow(try view.inspect().text())
+#endif
+    }
+    
+    func testHideIfCanImport() throws {
+        let view = Text("Hidden").hide(ifCanImport: .uiKit)
+        
+#if canImport(UIKit)
+        XCTAssertThrowsError(try view.inspect().text()) { error in
+            XCTAssertTrue(error is InspectionError)
+        }
+#else
+        XCTAssertNoThrow(try view.inspect().text())
+#endif
     }
 }
